4ae1556dc32eadfbce11d8335f8f623465e21ee2
Dennys Christian Mallqui Arguelles <dennys.mallqui@ibm.com>
2020-08-22 01:06:03 -0500

    final

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn.preprocessing import OrdinalEncoder
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -24,8 +24,30 @@ class Dummy(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X):
-        # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
-        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
         return data
+    
+    
+class ModelTransformer(TransformerMixin):
 
+    def __init__(self, model):
+        self.model = model
+
+    def fit(self, *args, **kwargs):
+        if len(args) > 1:
+            X_train, X_test, y_train, y_test = train_test_split(args[0], args[1], test_size=0.3, stratify=args[1].values, random_state=42)
+            self.model.fit(X_train, y_train, **kwargs)
+        else:            
+            X_train, X_test = train_test_split(args[0], test_size=0.3, random_state=42)
+            self.model.fit(X_train, **kwargs)        
+        return self
+
+    def transform(self, X, **transform_params):        
+        df = pd.DataFrame(self.model.predict(X))
+        for i in df.dtypes:
+            if str(i) == "object":
+                enc = OrdinalEncoder()
+                return enc.fit_transform(df)
+        return df  
+    
+    
