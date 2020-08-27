3da48845a649130802f98e899cdf7fdb694ee37b
Dennys Christian Mallqui Arguelles <dennys.mallqui@ibm.com>
2020-08-27 01:11:53 -0500

    ok

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -54,7 +54,7 @@ class ModelTransformer(TransformerMixin):
 
     def fit(self, *args, **kwargs):
         if len(args) > 1:
-            X_train, X_test, y_train, y_test = train_test_split(args[0], args[1], test_size=0.3, stratify=args[1].values, random_state=42)
+            X_train, X_test, y_train, y_test = train_test_split(args[0], args[1], test_size=0.2, stratify=args[1].values, random_state=42)
             self.model.fit(X_train, y_train, **kwargs)
         else:            
             X_train, X_test = train_test_split(args[0], test_size=0.3, random_state=42)
