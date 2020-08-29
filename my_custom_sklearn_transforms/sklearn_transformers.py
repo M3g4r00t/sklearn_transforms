858985d490bbd0b9135a56b7bfa0826976c5d33b
Dennys Christian Mallqui Arguelles <dennys.mallqui@ibm.com>
2020-08-29 15:47:21 -0500

    add

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -20,6 +20,23 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return data.drop(labels=self.columns, axis='columns')
 
     
+# All sklearn Transforms must have the `transform` and `fit` methods
+class SumColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, sum_column):
+        self.columns = columns
+        self.sum_column = sum_column
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        data[self.sum_column] = data[self.columns].sum(axis=1)
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
+        return data
+    
+    
 class NaNSearch(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
@@ -58,16 +75,22 @@ class ModelTransformer(TransformerMixin):
             X_train, X_test, y_train, y_test = train_test_split(args[0], args[1], test_size=0.2, stratify=args[1].values, random_state=42)
             self.model.fit(X_train, y_train, **kwargs)
         else:            
-            X_train, X_test = train_test_split(args[0], test_size=0.3, random_state=42)
+            X_train, X_test = train_test_split(args[0], test_size=0.2, random_state=42)
             self.model.fit(X_train, **kwargs)        
         return self
 
     def transform(self, X, **transform_params):        
-        df = pd.DataFrame(self.model.predict(X))
+        df = pd.DataFrame()
+        if callable(getattr(self.model, 'predict', None)):
+            df = pd.DataFrame(self.model.predict(X, **transform_params))
+        elif callable(getattr(self.model, 'transform', None)):
+            df = pd.DataFrame(self.model.transform(X, **transform_params))
+        elif callable(getattr(self.model, 'fit_transform', None)):
+            df = pd.DataFrame(self.model.fit_transform(X, **transform_params))
+                
         for i in df.dtypes:
             if str(i) == "object":
                 enc = OrdinalEncoder()
                 return enc.fit_transform(df)
         return df  
-    
-    
+
